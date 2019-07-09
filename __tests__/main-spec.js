1ac2e1d05b03eef114a8eff9c1c56f1bca5b05cf
ALVARLEUNG <alvar.leung@qq.com>
2019-07-09 18:03:23 +0800

    add functions and tests for -Implement-A-Multiplication-Table-

---
+++ __tests__/main-spec.js
@@ -0,0 +1,40 @@
+const main = require('../main');
+
+const isStartSmaller = main.isStartSmaller;
+const isOutOfRange = main.isOutOfRange;
+const createMultiplicationTable = main.createMultiplicationTable;
+
+// Test for isStartSmaller
+it ('should return true when the start smaller than the end given 2 and 4', () => {
+    expect(isStartSmaller(2, 4)).toBe(true);
+});
+
+// Test for isOutOfRange
+it ('should return true when the start out of range given -1 and 4', () => {
+    expect(isOutOfRange(-1, 4)).toBe(true);
+});
+
+it ('should return true when the end out of range given 1 and 1001', () => {
+    expect(isOutOfRange(1, 1001)).toBe(true);
+});
+
+it ('should return true when the start and the end in the range given 1 and 2', () => {
+    expect(isOutOfRange(1, 2)).toBe(false);
+});
+
+// Test for createMultiplicationTable
+it ('should return null when the start and the end out of the range given 0, 1001', () => {
+    expect(createMultiplicationTable(0, 1001)).toBe(null);
+});
+
+it ('should return null when start smaller than the end given 2, 4', () => {
+    expect(createMultiplicationTable(2, 4)).toBe(null);
+});
+
+it ('should return null when the end out of range given 1, 1001', () => {
+    expect(createMultiplicationTable(1, 1001)).toBe(null);
+});
+
+it ('should return result string when start smaller is equal the end given 2, 2', () => {
+    expect(createMultiplicationTable(2, 2)).toBe("2*2=4 /n");
+});
---
+++ main.js
@@ -0,0 +1,31 @@
+function isStartSmaller (start, end) {
+    return start <= end;
+}
+
+function isOutOfRange (start, end) {
+    if (start >= 1 && end <=1000 && start <= 1000 && end >=1)
+        return false;
+    else
+        return true;
+}
+
+function createMultiplicationTable (start, end) {
+    var multiplicationTabl = '';
+    if (!isOutOfRange && isStartSmaller) {
+        for (var n=start;n<=end;n++) {
+
+            for (var m=start;m<n+1;m++) {
+            var b=m*n;
+            
+            multiplicationTabl += m+"×"+n+"="+b+" "+" ";
+            
+            }
+            multiplicationTabl += "\n" ;
+            }
+            return multiplicationTabl
+    }else {
+        return null
+    }
+}
+
+module.exports = {isStartSmaller, isOutOfRange, createMultiplicationTable};
